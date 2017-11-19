5a4d9f0eae64e85c55a291c5a14f237d327a69c4
alucarth <ltorrezs2008@gmail.com>
2017-11-19 02:51:59 -0400

    crud vuejs

--- index.php
+++ index.php
@@ -3,18 +3,63 @@
   <head>
     <meta charset="utf-8">
     <title>Vue Example</title>
-    <script src="build/app.js"></script>
+    <script src="https://use.fontawesome.com/5e17fffd96.js"></script>
   </head>
   <body>
-	  	<div class="container">
+      <div id="vue-app" class="container">
 
-	  		<h3>practicando vue </h3>
-	  		<input type="text" class="form-control"><br>
+        <h3> Tereas </h3>
 
-	  		<button class="btn btn-success">boton hdp</button>
+        <ul class="list-group task">
+            <li v-for="(tarea, index) in tareas" class="list-group-item">
+            <a  @click="toggleStatus(tarea)" ><span class="fa" :class="tarea.pendiente?'fa-square-o':'fa-check-square-o'" ></span></a>
+            
 
-	  	</div>
+              <template v-if="!tarea.editando">
+                
+                &nbsp&nbsp<span>{{tarea.descripcion}}</span> 
 
-	  
+                <div>
+                  &nbsp&nbsp
+                  <a  @click="editarTarea(tarea)" ><span class="fa fa-edit" ></span></a>
+                  &nbsp&nbsp
+                  <a @click="eliminarTarea(index)" ><span class="fa fa-trash" ></span></a>
+                  </li>
+                </div>
+
+              </template>
+
+              <template v-else>
+                
+                <input type="text" class="form-control" name="" v-model="tarea_backup">
+
+                <div>
+                 
+                  <a @click="actualizarTarea(tarea)"><span class="fa fa-check" ></span></a>
+                  &nbsp&nbsp
+                  <a @click="cancelarTarea(tarea)"><span class="fa fa-close" ></span></a>
+                  </li>
+                </div> 
+              </template>
+
+              
+            
+           
+        </ul>
+        <br>
+        <button class="btn btn-danger" type="button" @click="borrarTareas"> Borrar Todo </button>
+
+
+        <form @submit.prevent="crearTarea">
+          <br>
+          <input v-model="nueva_tarea" ype="text" name="" class="form-control">
+          <br>
+          <button  type="submit" class="btn btn-primary">Crear Tarea</button>
+          
+        </form>
+      </div>
+
+  <script src="build/app.js"></script>
+
   </body>
 </html>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,83 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
 import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
+
+
+var vue = new Vue({
+  el: '#vue-app',
+  methods:{
+  	crearTarea: function(){
+  		this.tareas.push({
+  			descripcion: this.nueva_tarea,
+  			pendiente: true,
+  			editando: false
+  		});
+
+  		this.nueva_tarea='';
+  	},
+  	toggleStatus: function(tarea){
+  		tarea.pendiente =!tarea.pendiente;
+
+  	},
+  	editarTarea: function(tarea){
+  		this.tareas.forEach(function(tarea){
+  		tarea.editando=false;
+  			
+  		});
+  		this.tarea_backup=tarea.descripcion;
+  		tarea.editando=true;
+  	},
+  	cancelarTarea: function(tarea){
+  		tarea.editando=false;
+  	},
+  	actualizarTarea: function(tarea){
+  		
+  		tarea.descripcion = this.tarea_backup;
+  		tarea.editando = false;
+
+  	},
+  	eliminarTarea: function(index){
+  		this.tareas.splice(index,1);
+  	},
+  	borrarTareas: function(){
+  		this.tareas = this.tareas.filter(function(tarea){
+  			return tarea.pendiente;
+  		});
+  	}
+
+  },
+  data:{
+  	nueva_tarea: '',
+  	tarea_backup: '',
+  	tareas: [
+  		{
+	  		descripcion:'aprender vue 1',
+	  		pendiente: true,
+	  		editando: false
+  		},
+  		{
+	  		descripcion:'Dormir 2',
+	  		pendiente: true,
+	  		editando: false
+  		},
+  		{
+	  		descripcion:'Call Elu',
+	  		pendiente: false,
+	  		editando: false
+  		}
+
+  	],
+  	name : 'David '
+  },
+  computed: {
+    // a computed getter
+    reversedMessage: function () {
+      // `this` points to the vm instance
+      return this.name.split('').reverse().join('')
+    }
+  }
+}); 
