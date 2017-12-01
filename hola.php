30c5590cd49b96ea3037b286decd3f64e41241b0
alucarth <ltorrezs2008@gmail.com>
2017-12-01 18:30:13 -0400

    adicionando hola

---
+++ hola.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOla mundo </title>
+</head>
+<body>
+	<label>Hola Muchachos esto es un test</label>
+	<input type="text" class="form-control">
+	<script src="build/app.js"></script>
+</body>
+</html>
